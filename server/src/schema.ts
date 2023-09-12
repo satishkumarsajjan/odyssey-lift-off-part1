1e23f63e905f1f36922dc45a658f5c8be7acedd6
satishkumar <satishsajjan4564@gmail.com>
2023-09-12 12:25:59 +0530

    schema

---
+++ server/src/schema.ts
@@ -0,0 +1,26 @@
+import gql from 'graphql-tag';
+export const typeDefs = gql`
+  # Schema definitions go here
+
+  type Query {
+    "Get tracks array for homepage grid"
+    tracksForHome: [Track!]!
+  }
+
+  "A track is a group of Modules that teaches about a specific topic"
+  type Track {
+    id: ID!
+    title: String!
+    author: Author!
+    thumbnail: String
+    length: Int
+    modulesCount: Int
+  }
+
+  "Author of a complete Track or a Module"
+  type Author {
+    id: ID!
+    name: String!
+    photo: String
+  }
+`;
