1d9b70a9111adb0b42ff72d58b89480d1803345c
satishkumar <satishsajjan4564@gmail.com>
2023-09-26 16:41:58 +0530

    cards rendered, basics done

--- client/src/components/query-result.tsx
+++ client/src/components/query-result.tsx
@@ -1,27 +1,31 @@
-import React, { PropsWithChildren} from 'react';
+import React, { PropsWithChildren } from 'react';
 import styled from '@emotion/styled';
 import { LoadingSpinner } from '@apollo/space-kit/Loaders/LoadingSpinner';
-import { ApolloError } from '@apollo/client'
-
+import { ApolloError } from '@apollo/client';
 
 interface QueryResultProps {
   loading: boolean;
   error?: ApolloError | undefined;
-  data?: unknown,
+  data?: unknown;
 }
 
 /**
  * Query Results conditionally renders Apollo useQuery hooks states:
  * loading, error or its children when data is ready
  */
-const QueryResult: React.FC<PropsWithChildren<QueryResultProps>> = ({ loading, error, data, children }): React.ReactElement<any, any> | null => {
+const QueryResult: React.FC<PropsWithChildren<QueryResultProps>> = ({
+  loading,
+  error,
+  data,
+  children,
+}): React.ReactElement<any, any> | null => {
   if (error) {
     return <p>ERROR: {error.message}</p>;
   }
   if (loading) {
     return (
       <SpinnerContainer>
-        <LoadingSpinner data-testid="spinner" size="large" theme="grayscale" />
+        <LoadingSpinner data-testid='spinner' size='large' theme='grayscale' />
       </SpinnerContainer>
     );
   }
@@ -29,8 +33,7 @@ const QueryResult: React.FC<PropsWithChildren<QueryResultProps>> = ({ loading, e
     return <>{children}</>;
   }
 
-    return <p>Nothing to show...</p>;
-  
+  return <p>Nothing to show...</p>;
 };
 
 export default QueryResult;
